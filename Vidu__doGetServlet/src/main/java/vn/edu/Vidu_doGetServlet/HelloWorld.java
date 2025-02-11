b7674b429798741ad73911391ccd725bd55384cd
ptai82067 <ptai82067@gmail.com>
2025-02-11 18:01:04 +0700

    Vidu goGet() Servlet

---
+++ Vidu__doGetServlet/src/main/java/vn/edu/Vidu_doGetServlet/HelloWorld.java
@@ -0,0 +1,34 @@
+package vn.edu.Vidu_doGetServlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter dau_ghi_kq = response.getWriter();
+		dau_ghi_kq.print("Hello Pham Phuoc Tai");
+	}
+
+}
