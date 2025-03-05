508bde102e053fe4d4eb76d8c93da8e0d5536df7
ptai82067 <ptai82067@gmail.com>
2025-03-05 19:24:29 +0700

    TH1_SpringBoot

---
+++ HelloSpringBoot/src/main/java/edu/taipp/HelloSpringBoot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package edu.taipp.HelloSpringBoot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/chao")
+	public String xin_chao(ModelMap m) {
+		m.addAttribute("tb", "Dữ liệu thông báo xin chào");
+		return "welcome";
+	}
+}
