e9abae6723bf31f2acfcd3f88f0351f6c66fea0e
ptai82067 <ptai82067@gmail.com>
2025-03-05 19:24:40 +0700

    .

---
+++ TestSpringBoot/TestSpringBoot/src/test/java/edu/taipp/TestSpringBoot/TestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.taipp.TestSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
