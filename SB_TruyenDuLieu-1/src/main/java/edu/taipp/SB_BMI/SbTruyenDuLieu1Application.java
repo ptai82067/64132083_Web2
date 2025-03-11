c04852d25af844cc1ae0e4999aa2e58fb654ee0f
ptai82067 <ptai82067@gmail.com>
2025-03-11 13:48:41 +0700

    .

---
+++ SB_TruyenDuLieu-1/src/main/java/edu/taipp/SB_BMI/SbTruyenDuLieu1Application.java
@@ -0,0 +1,13 @@
+package edu.taipp.SB_BMI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbTruyenDuLieu1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbTruyenDuLieu1Application.class, args);
+	}
+
+}
---
+++ SB_TruyenDuLieu-1/src/test/java/edu/taipp/SB_BMI/SbTruyenDuLieu1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.taipp.SB_BMI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTruyenDuLieu1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
