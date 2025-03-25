0e6713260404118e6719e43e6c740cd9824afa5f
ptai82067 <ptai82067@gmail.com>
2025-03-25 09:54:44 +0700

    THGK

--- TongHopGK/src/main/java/edu/taipp/SB_TongHopGK/controller/HomeController.java
+++ TongHopGK/src/main/java/edu/taipp/SB_TongHopGK/controller/HomeController.java
@@ -1,5 +1,32 @@
 package edu.taipp.SB_TongHopGK.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String homePage() {
+		
+		return "home";
+	}
+	
+	@RequestMapping("/about")
+	public String aboutPage() {
+		
+		return "about";
+	}
+	
+	@RequestMapping("/student-list")
+	public String listPage() {
+		
+		return "students";
+	}
+	
+	@RequestMapping("/add-new")
+	public String addNewPage() {
+		
+		return "add_new";
+	}
+	
 }
