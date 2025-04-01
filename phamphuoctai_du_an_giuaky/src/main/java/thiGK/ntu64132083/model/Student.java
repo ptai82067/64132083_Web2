db4bc27e0093b26c3eccc6e96c06cd78e0948366
ptai82067 <ptai82067@gmail.com>
2025-04-01 09:26:35 +0700

    ____THI GIỮA KỲ_____

---
+++ phamphuoctai_du_an_giuaky/src/main/java/thiGK/ntu64132083/model/Student.java
@@ -0,0 +1,32 @@
+package thiGK.ntu64132083.model;
+
+public class Student {
+	private String id;
+	private String name;
+	private String groupId;
+	public Student(String id, String name, String groupId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.groupId = groupId;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGroupId() {
+		return groupId;
+	}
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+	
+}
---
+++ phamphuoctai_du_an_giuaky/src/main/java/thiGK/ntu64132083/model/Topic.java
@@ -0,0 +1,48 @@
+package thiGK.ntu64132083.model;
+
+public class Topic {
+	private String id;
+	private String topicName;
+	private String topicDescription;
+	private String supervisorId;
+	private String topicType;
+	public Topic(String id, String topicName, String topicDescription, String supervisorId, String topicType) {
+		super();
+		this.id = id;
+		this.topicName = topicName;
+		this.topicDescription = topicDescription;
+		this.supervisorId = supervisorId;
+		this.topicType = topicType;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTopicName() {
+		return topicName;
+	}
+	public void setTopicName(String topicName) {
+		this.topicName = topicName;
+	}
+	public String getTopicDescription() {
+		return topicDescription;
+	}
+	public void setTopicDescription(String topicDescription) {
+		this.topicDescription = topicDescription;
+	}
+	public String getSupervisorId() {
+		return supervisorId;
+	}
+	public void setSupervisorId(String supervisorId) {
+		this.supervisorId = supervisorId;
+	}
+	public String getTopicType() {
+		return topicType;
+	}
+	public void setTopicType(String topicType) {
+		this.topicType = topicType;
+	}
+	
+}
