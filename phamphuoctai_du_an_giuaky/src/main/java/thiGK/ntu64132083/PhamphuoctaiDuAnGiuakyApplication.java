11204f1d04754c8e289f0f78988b8b4922243644
ptai82067 <ptai82067@gmail.com>
2025-04-01 09:01:13 +0700

    ______THI GIỮA KỲ______

---
+++ phamphuoctai_du_an_giuaky/src/main/java/thiGK/ntu64132083/PhamphuoctaiDuAnGiuakyApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64132083;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamphuoctaiDuAnGiuakyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamphuoctaiDuAnGiuakyApplication.class, args);
+	}
+
+}
---
+++ phamphuoctai_du_an_giuaky/src/test/java/thiGK/ntu64132083/PhamphuoctaiDuAnGiuakyApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64132083;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamphuoctaiDuAnGiuakyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
