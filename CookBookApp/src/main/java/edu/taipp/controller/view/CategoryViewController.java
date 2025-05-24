4008ee3301293f21a45669c463524dfb36fdb6d1
ptai82067 <ptai82067@gmail.com>
2025-05-24 23:57:34 +0700

    crud Category

--- CookBookApp/src/main/java/edu/taipp/controller/view/CategoryViewController.java
+++ CookBookApp/src/main/java/edu/taipp/controller/view/CategoryViewController.java
@@ -1,11 +1,11 @@
 package edu.taipp.controller.view;
 
+import edu.taipp.model.Category;
 import edu.taipp.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin/categories")
@@ -21,5 +21,39 @@ public class CategoryViewController {
   public String getAllCategories(Model model) {
     model.addAttribute("categories", categoryService.getAllCategorys());
     return "admin/categories/index"; // Tên file template (categories.html)
+  }
+  // Hiển thị form tạo danh mục mới
+  @GetMapping("/create")
+  public String showCreateForm(Model model) {
+    model.addAttribute("category", new Category());
+    return "admin/categories/category-form";
+  }
+
+  // Xử lý tạo danh mục mới
+  @PostMapping("/create")
+  public String createCategory(@ModelAttribute("category") Category category) {
+    categoryService.createCategory(category);
+    return "redirect:/admin/categories";
+  }
+
+  // Hiển thị form chỉnh sửa danh mục
+  @GetMapping("/edit/{id}")
+  public String showEditForm(@PathVariable Long id, Model model) {
+    Category category = categoryService.getCategoryById(id);
+    model.addAttribute("category", category);
+    return "admin/categories/category-form";
+  }
+
+  // Xử lý chỉnh sửa danh mục
+  @PostMapping("/edit/{id}")
+  public String updateCategory(@PathVariable Long id, @ModelAttribute("category") Category category) {
+    categoryService.updateCategory(id, category);
+    return "redirect:/admin/categories";
+  }
+
+  @GetMapping("/delete/{id}")
+  public String deleteCategory(@PathVariable Long id) {
+    categoryService.deleteCategory(id);
+    return "redirect:/admin/categories";
   }
 }
